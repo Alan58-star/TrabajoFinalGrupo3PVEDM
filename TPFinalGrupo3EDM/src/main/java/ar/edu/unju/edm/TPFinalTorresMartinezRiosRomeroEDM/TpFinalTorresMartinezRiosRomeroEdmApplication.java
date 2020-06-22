fddf44626c62e8dc1a46590e27755c20c5fae5af
NT125 <62914776+NT125@users.noreply.github.com>
2020-06-22 11:31:39 -0300

    Actualizado pom.xml, Spring Boot añadido

---
+++ TPFinalGrupo3EDM/src/main/java/ar/edu/unju/edm/TPFinalTorresMartinezRiosRomeroEDM/TpFinalTorresMartinezRiosRomeroEdmApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.edm.TPFinalTorresMartinezRiosRomeroEDM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpFinalTorresMartinezRiosRomeroEdmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpFinalTorresMartinezRiosRomeroEdmApplication.class, args);
+	}
+
+}
